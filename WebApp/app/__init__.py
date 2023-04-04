86d610c558b010fd2d7425e60b49228a8eedb0d8
Patryk Partyka <patrykp4888@gmail.com>
2023-04-04 11:39:47 +0200

    [FEAT] User-login-logic-implemented

--- WebApp/app/__init__.py
+++ WebApp/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 from app.config import Config
 
@@ -18,8 +19,20 @@ bcrypt = Bcrypt(flask_app)
 db.init_app(app=flask_app)
 migrate.init_app(app=flask_app, db=db)
 
-from .views import IndexView, RegisterView, LoginView
+from .views import IndexView, RegisterView, LoginView, LogoutView, HomeView
 
 flask_app.add_url_rule('/', view_func=IndexView.as_view('index'), methods=["GET"])
+flask_app.add_url_rule('/home', view_func=IndexView.as_view('home'), methods=["GET"])
 flask_app.add_url_rule('/register', view_func=RegisterView.as_view('register'), methods=["GET", "POST"])
 flask_app.add_url_rule('/login', view_func=LoginView.as_view('login'), methods=["GET", "POST"])
+flask_app.add_url_rule('/logout', view_func=LoginView.as_view('logout'), methods=["GET"])
+
+from .models import Users
+
+login_manager = LoginManager()
+login_manager.login_view = 'views.login'
+login_manager.init_app(flask_app)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
--- WebApp/app/forms.py
+++ WebApp/app/forms.py
@@ -6,3 +6,8 @@ class RegisterForm(Form):
     email = EmailField('Email', [validators.Length(min=6, max=120), validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm_password', message='Password must match!')])
     confirm_password = PasswordField('Repeat password')
+
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
