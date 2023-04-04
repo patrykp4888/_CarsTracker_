0b22fbae75a767c2fbb3a19681371d7aff56e544
Patryk Partyka <patrykp4888@gmail.com>
2023-04-04 15:23:37 +0200

    [FEAT] Models-constraints-fixed-and-db-upgraded

--- WebApp/app/models.py
+++ WebApp/app/models.py
@@ -8,8 +8,8 @@ class Users(db.Model, UserMixin):
     __tablename__ = "Users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    surname = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), nullable=True)
+    surname = db.Column(db.String(100), nullable=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(250), nullable=False)    
     email = db.Column(db.String(120), unique=True, nullable=False)
