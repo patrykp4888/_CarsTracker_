91ab398ab7fc73174c68de19c6b02bb8b5e88d2e
Patryk Partyka <patrykp4888@gmail.com>
2023-04-17 17:42:21 +0200

    [FEAT] New-DB-init-migrate-update

--- web_app/app/config.py
+++ web_app/app/config.py
@@ -6,5 +6,5 @@
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost:5432/users"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost:5432/cars_tracker"
     SECRET_KEY = "secretkey"
